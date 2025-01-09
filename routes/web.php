9ab16eedc477a2a7e85a37c0160393153d39fd3b
Afieee <afiwijaya555@gmail.com>
2025-01-09 10:42:23 +0700

    Adding middleware routing

--- routes/web.php
+++ routes/web.php
@@ -10,38 +10,30 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostLikeController;
 
 Route::get('/', [UsersController::class, 'halamanIndex']);
-
-Route::get('/policy', [UsersController::class, 'halamanPolicy']);
-
-
 Route::get('/login', function () {
     return view('auth.login');
 });
-
 Route::get('/register', function () {
     return view('auth.register');
 });
-
 Route::post('/register-berhasil', [\App\Http\Controllers\UsersController::class, 'register']);
-
 Route::post('/login-berhasil', [\App\Http\Controllers\UsersController::class, 'login']);
-Route::post('/logout', [UsersController::class, 'logout']);
-
-Route::get('/home', [\App\Http\Controllers\UsersController::class, 'halamanHome'])->name('halaman-home');
 
 
-Route::get('/upload', [ImageController::class, 'halamanCreate'])->name('image.index');
-Route::post('/process', [ImageController::class, 'process'])->name('image.process');
-Route::post('/image/store', [ImageController::class, 'store'])->name('post.store');
-Route::post('/toggle-like', [PostLikeController::class, 'toggleLike'])->name('toggle-like');
-Route::post('/follow/{user_id}', [FollowController::class, 'toggleFollow'])->name('follow.toggle');
 
 
-Route::get('/comment/{id_post}', [CommentController::class, 'halamanComment'])->name('comment');
-
-Route::post('/commented', [CommentController::class, 'tambahComment']);
-
-Route::get('/profile/{user_id}', [ProfileController::class, 'getProfile'])->name('profile');
-Route::get('/setting/{sessionId}', [ProfileController::class, 'setting'])->name('setting');
-
-Route::put('/edit-profile/{sessionId}', [ProfileController::class, 'updateProfile'])->name('updateProfile');
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [\App\Http\Controllers\UsersController::class, 'halamanHome'])->name('halaman-home');
+    Route::get('/policy', [UsersController::class, 'halamanPolicy']);
+    Route::get('/upload', [ImageController::class, 'halamanCreate'])->name('image.index');
+    Route::post('/process', [ImageController::class, 'process'])->name('image.process');
+    Route::post('/image/store', [ImageController::class, 'store'])->name('post.store');
+    Route::post('/toggle-like', [PostLikeController::class, 'toggleLike'])->name('toggle-like');
+    Route::post('/follow/{user_id}', [FollowController::class, 'toggleFollow'])->name('follow.toggle');
+    Route::get('/comment/{id_post}', [CommentController::class, 'halamanComment'])->name('comment');
+    Route::post('/commented', [CommentController::class, 'tambahComment']);
+    Route::get('/profile/{user_id}', [ProfileController::class, 'getProfile'])->name('profile');
+    Route::get('/setting/{sessionId}', [ProfileController::class, 'setting'])->name('setting');
+    Route::put('/edit-profile/{sessionId}', [ProfileController::class, 'updateProfile'])->name('updateProfile');
+    Route::post('/logout', [UsersController::class, 'logout']);
+});
