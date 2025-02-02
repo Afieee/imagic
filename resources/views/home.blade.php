aff4995c74b680c46a4a37038f72a2d3f7834203
Afieee <afiwijaya555@gmail.com>
2025-02-02 20:17:16 +0700

    Finalizing the project post hosting part 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -75,11 +75,21 @@
                     <span class="like-count">Liked by <strong
                             style="font-weight: 900; font-size: 1.0em; color: #555;">{{ $item->post_likes->count() }}</strong>
                         others</span>
-                    @if ($item->comment->count() > 0)
+                    @if ($item->comment->count() == 1)
                         <a href="{{ route('comment', $item->id_post) }}" style="text-decoration: none">
-                            <span class="like-count comment-count">View <strong
-                                    style="font-weight: 900; font-size: 1.0em; color: #555;">{{ $item->comment->count() }}</strong>
-                                Comments</span>
+                            <span class="like-count comment-count">
+                                View <strong style="font-weight: 900; font-size: 1.0em; color: #555;">
+                                    {{ $item->comment->count() }}
+                                </strong> Comment
+                            </span>
+                        </a>
+                    @elseif ($item->comment->count() > 1)
+                        <a href="{{ route('comment', $item->id_post) }}" style="text-decoration: none">
+                            <span class="like-count comment-count">
+                                View <strong style="font-weight: 900; font-size: 1.0em; color: #555;">
+                                    {{ $item->comment->count() }}
+                                </strong> Comments
+                            </span>
                         </a>
                     @endif
 
